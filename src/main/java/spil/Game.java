f177ced9920bee84d4bb9027e86d0745e239d56e
Oliver <32456406+outrageousbog@users.noreply.github.com>
2017-11-27 16:18:33 +0100

    Edit

--- src/main/java/spil/Game.java
+++ src/main/java/spil/Game.java
@@ -1,7 +1,10 @@
 package spil;
 
+import gui_fields.GUI_Player;
 import gui_main.GUI;
 import spil.Board.Board;
+
+import java.util.ArrayList;
 
 
 public class Game {
@@ -19,6 +22,11 @@ public class Game {
 
     public void play()
     {
+        int numberOfPlayers = this.gui.getUserInteger("<PLACEHOLDER> min. 2, max 4",2,4);
+        ArrayList<Player> player = new ArrayList<>();
+        
+
+
 
     }
 
