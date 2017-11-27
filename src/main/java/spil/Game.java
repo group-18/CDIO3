3c1d4e4ea1afb0c94e382fd8b7ae4c502bd0c57b
Magnus Hansen <magnus.h1993@gmail.com>
2017-11-27 14:31:26 +0100

    Add main methods for Game

--- src/main/java/spil/Game.java
+++ src/main/java/spil/Game.java
@@ -1,10 +1,31 @@
 package spil;
 
+import gui_main.GUI;
+import spil.Board.Board;
+
 
 public class Game {
 
-    public static void main(String[] args) {
+    private Board board;
+    private GUI gui;
 
+
+    private Game()
+    {
+        this.board = new Board();
+        this.gui = new GUI(this.board.getGuiFields());
+    }
+
+
+    public void play()
+    {
+
+    }
+
+
+    public static void main(String[] args)
+    {
+        new Game().play();
     }
 
 }
