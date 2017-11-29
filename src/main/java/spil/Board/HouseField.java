9ee12559910def81d0b01b7422be8fe38eef15f0
sebdtu <32480015+sebdtu@users.noreply.github.com>
2017-11-29 14:22:56 +0100

    Added method runAction to Housefield

--- src/main/java/spil/Board/HouseField.java
+++ src/main/java/spil/Board/HouseField.java
@@ -17,6 +17,8 @@ public class HouseField extends Field {
      * Is {@code null} when not owned.
      */
     private Player owner;
+    private HouseField houseField;
+
 
 
     /**
@@ -26,8 +28,7 @@ public class HouseField extends Field {
      * @param name The title of this HouseField
      * @param rent The rent of this HouseField
      */
-    public HouseField(String name, String rent)
-    {
+    public HouseField(String name, String rent) {
         this(name, "", "", rent, Color.BLUE, Color.BLACK);
     }
 
@@ -36,15 +37,14 @@ public class HouseField extends Field {
      * Constructs a HouseField with all needed information; Name, description,
      * sub description, rent, background color and foreground color
      *
-     * @param name The name of this HouseField
-     * @param description The description of this HouseField (shown in the card in the middle of the board)
-     * @param subDescription The sub description of this HouseField (shown in the actual field on the board)
-     * @param rent The rent of this HouseField
+     * @param name            The name of this HouseField
+     * @param description     The description of this HouseField (shown in the card in the middle of the board)
+     * @param subDescription  The sub description of this HouseField (shown in the actual field on the board)
+     * @param rent            The rent of this HouseField
      * @param backgroundColor The color of this HouseField background
      * @param foregroundColor The color of this HouseField foreground
      */
-    public HouseField(String name, String description, String subDescription, String rent, Color backgroundColor, Color foregroundColor)
-    {
+    public HouseField(String name, String description, String subDescription, String rent, Color backgroundColor, Color foregroundColor) {
         super(name, description, subDescription, backgroundColor, foregroundColor);
 
         this.rent = rent;
@@ -57,8 +57,7 @@ public class HouseField extends Field {
      * {@inheritDoc}
      */
     @Override
-    protected GUI_Street createGUIFIeld()
-    {
+    protected GUI_Street createGUIFIeld() {
         return new GUI_Street();
     }
 
@@ -67,8 +66,7 @@ public class HouseField extends Field {
      * {@inheritDoc}
      */
     @Override
-    public GUI_Street getGuiField()
-    {
+    public GUI_Street getGuiField() {
         return (GUI_Street) this.guiField;
     }
 
@@ -78,19 +76,20 @@ public class HouseField extends Field {
      *
      * @return The rent for this HouseField
      */
-    public String getRent()
-    {
+    public String getRent() {
         return this.rent;
     }
 
+    private int rentToInt(String rent) {
+        return Integer.parseInt(rent);
+    }
 
     /**
      * Method to determine if this HouseField is owned
      *
      * @return Is this HouseField owned?
      */
-    public boolean isOwned()
-    {
+    public boolean isOwned() {
         return this.owner != null;
     }
 
@@ -101,8 +100,7 @@ public class HouseField extends Field {
      * @param player The Player to check with
      * @return If this HouseField is owned by Player
      */
-    public boolean isOwnedByPlayer(Player player)
-    {
+    public boolean isOwnedByPlayer(Player player) {
         return this.owner == player;
     }
 
@@ -112,9 +110,28 @@ public class HouseField extends Field {
      *
      * @param player The Player to own this HouseField
      */
-    public void setOwner(Player player)
-    {
+    public void setOwner(Player player) {
         this.owner = player;
     }
 
-}
+    public Player getOwner() {
+        return this.owner;
+    }
+
+
+
+    public void runAction(Player player, Field field) {
+        HouseField houseField = HouseField.class.cast(field);
+        if (houseField.isOwned()){
+                if (!isOwnedByPlayer(player)){
+                    player.addBalance(-houseField.rentToInt(getRent()));
+                    houseField.getOwner().addBalance(houseField.rentToInt(getRent()));
+                }
+            }
+            else {
+                player.addBalance(-houseField.rentToInt(getRent()));
+                houseField.setOwner(player);
+            }
+        }
+    }
+
