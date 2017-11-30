d7a6f992025adb412194760c4cdea95b7c9e3f80
Dino1106 <niklaes@gmail.com>
2017-11-30 20:38:12 +0100

    FreeCard 4 Jail - Cleanup

--- src/main/java/spil/Game.java
+++ src/main/java/spil/Game.java
@@ -203,7 +203,6 @@ public class Game {
         Field field = this.board.getPlayerField(currentPlayer);
         field.runAction(this);
         isCurrentPlayerBankupt();
-        System.out.println(getCurrentPlayer().getName() + getCurrentPlayer().getPrisonCard());
     }
 
     /**
